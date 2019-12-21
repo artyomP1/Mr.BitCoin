0164dba10da45f3bf8e857a3d94ae715cc215ace
Artyom Pogosov <artyomp@outlook.com>
2019-12-21 18:26:11 +0200

    Updates

---
+++ precache-manifest.87fcf09ae226b6d5fa289613ba0a55dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2587391699d39f7ac9c76f28a1ab37e0",
+    "url": "/artyomP1/Mr.BitCoin/deployments/index.html"
+  },
+  {
+    "revision": "7f0ae6341e37f906507f",
+    "url": "/artyomP1/Mr.BitCoin/deployments/static/css/main.427dfff1.chunk.css"
+  },
+  {
+    "revision": "197ab875f2d43f49493d",
+    "url": "/artyomP1/Mr.BitCoin/deployments/static/js/2.114cf39c.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/artyomP1/Mr.BitCoin/deployments/static/js/2.114cf39c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f0ae6341e37f906507f",
+    "url": "/artyomP1/Mr.BitCoin/deployments/static/js/main.dad226d6.chunk.js"
+  },
+  {
+    "revision": "ee137f37536a29f09e0c",
+    "url": "/artyomP1/Mr.BitCoin/deployments/static/js/runtime-main.b9ff4a27.js"
+  }
+]);
